4ee7461fde44a592f63500c711b643356cae2b89
CRKarur <chandrikarathnakarur@gmail.com>
2021-03-21 22:04:11 -0400

    sample code

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+  res.send('EMR');
+});
 
-app.get('/info', (req,res) =>{
-    res.send('information');
-})
+app.get('/About', (req,res) =>{
+    res.send('About Clinic');
+});
+
+app.get('/About', (req,res) =>{
+    res.send('About Clinic');
+});
+
+app.get('/SearchClinic', (req,res) =>{
+    res.send('SearchClinic');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
