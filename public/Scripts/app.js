e886adae3bcf07a96539b15e18233864b1d64acd
CRKarur <chandrikarathnakarur@gmail.com>
2021-03-23 10:03:48 -0400

    first set of code with view pages

---
+++ public/Scripts/app.js
@@ -0,0 +1,11 @@
+//IIFE -- immediately invoked function expression
+(function(){
+
+    function Start()
+    {
+        console.log("App Started.....");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET Home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET About page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET Search Clinics page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Search Clinics' });
+});
+
+/* GET Register page. */
+router.get('/register', function(req, res, next) {
+  res.render('index', { title: 'Register' });
+});
+
+/* GET Login page. */
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: 'Login' });
 });
 
 module.exports = router;
